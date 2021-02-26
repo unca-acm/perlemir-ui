685da4b469ab521dff4243337d25ff9c40f48532
mdip226 <mdip226@gmail.com>
2021-02-25 19:12:23 -0500

    static

--- lib/transform/src/DynamicMemory/dynamic_memory.c
+++ lib/transform/src/DynamicMemory/dynamic_memory.c
@@ -4,13 +4,13 @@ WASM_import unsigned char __heap_base;
 WASM_import unsigned char __private_heap_base;
 
 
-i32u shared_heap_pointer =  &__heap_base;
-i32u private_heap_pointer = &__private_heap_base;
+static i32u shared_heap_pointer = &__heap_base;
+static i32u private_heap_pointer = &__private_heap_base;
 
 // bytes: number of bytes you would like to allocate
 // private: 1 if you want to use the private heap,
 // 0 if you are using the shared heap.
-WASM_export void* stack_malloc(i32u bytes, i32 private) {
+void* stack_malloc(i32u bytes, i32 private) {
     
     if (private) {
         i32u mem = private_heap_pointer;
@@ -23,7 +23,7 @@ WASM_export void* stack_malloc(i32u bytes, i32 private) {
     }
 }
 
-WASM_export void pop_free(i32u bytes, i32 private){
+void pop_free(i32u bytes, i32 private){
     if (private)
     {
         private_heap_pointer -= bytes;
