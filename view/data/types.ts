178792a2fddeafc3b72ac5d3130e563254769fcf
Josh C <jtcooper90@gmail.com>
2021-02-26 13:54:51 -0500

    More helpful comments for the data context types.

--- view/data/types.ts
+++ view/data/types.ts
@@ -9,16 +9,27 @@ type DataBlockTitle = "historical";
 
 // Data Store context types
 interface StoragePropDefinitions {
-    // TODO
     instance?: WebAssembly.Instance;
 }
 export type StorageProps = React.PropsWithChildren<StoragePropDefinitions>
 export type Storage = React.FC<StorageProps>;
+
+/* DataBlock: represents a collection of all "raw" data for a given purpose.
+ * Domain can be stored as a separate variable in cases where it's necessary,
+ *   but not necessary for the underlying computations.
+ * TODO: add other important properties, maybe available queries/transforms?
+ */
 export interface DataBlock {
     data: BaseData;
     domain?: Int32Array;
 }
+
+/* Storage containing indexable blocks for different purposes.
+ * For example, you may have a different block for the price graph and candlestick data.
+ */
 export type DataBlockIndex = { [ title in DataBlockTitle ]: DataBlock };
+
+/* Properties held by the DataStore context API. */
 export interface StorageContext {
     dataBlock: DataBlockIndex;
     query: (target: BaseData) => { [q: string]: () => void }
